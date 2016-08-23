995f7c59d691eaad38a9fd0f37ded2c3973e42dc
Yong Jun <yjthong.2009@smu.edu.sg>
2016-08-23 11:49:56 +0800

    Use React production build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -28,7 +28,12 @@ module.exports = {
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
   ],
   devtool: 'source-map'
 };
